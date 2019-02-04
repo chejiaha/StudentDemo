4d26f18397f4eba60ba63f176cc7948c5b8b55f1
John <chejiaha@sheridancollege.ca>
2019-02-04 12:58:25 -0500

    adding comments while in John_Chen branch

--- src/StudentDemo/Student.java
+++ src/StudentDemo/Student.java
@@ -14,6 +14,7 @@ public class Student {
     private String studName;
     private String program;
     private String course;
+    //adding comment while in John_Chen branch
     
 public Student (){
     studId = 0;
